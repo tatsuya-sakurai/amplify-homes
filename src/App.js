a5e02647924074d58969a0f2d7dd493d1933ccb3
Tatsuya <tatsuya.sakurai@advance-jpn.com>
2023-08-28 16:19:35 +0900

    Appクラスの変更

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+  render() {
     <div className="App">
+      <AmplifySignOut />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <h1>Sample App</h1>
+      <p>これは、サンプルで作成したアプリです。</p>
     </div>
-  );
+  }
 }
 
 export default App;
